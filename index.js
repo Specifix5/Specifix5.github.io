08e680ce54c63a037d7a6aae732a89eb046d404a
= <62927726+Specifix5@users.noreply.github.com>
2023-06-12 21:34:53 +0700

    Optimized html

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-function mainSetup() {
+$(document).ready(function() {
     $.getJSON("https://api.ipify.org?format=json", function(data) {
         document.getElementById("ipText").innerHTML = '<span style="font-size:20px;">Hello ' + `<span style="font-family: 'Fira Code';border-right: 0.65em solid #eeeeee;animation: blink-caret 1s step-end infinite;">${data.ip}</span></span>`;
     });
-
     document.getElementById("prYear").innerText = `${(new Date()).getFullYear() - 2018} years`
-
     if (navigator.userAgent.match(/Android/i)
         || navigator.userAgent.match(/webOS/i)
         || navigator.userAgent.match(/iPhone/i)
@@ -18,4 +16,4 @@ function mainSetup() {
         document.getElementById("avatar_img").style.marginLeft = "75px";
         document.getElementById("project-text-wrapper").style.width = "35%";
     }
-}
+});
