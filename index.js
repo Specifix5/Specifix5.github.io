8b01b4b17064beebc991618605ed319f4ff63a01
= <62927726+Specifix5@users.noreply.github.com>
2023-07-23 18:27:18 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
-    $.getJSON("https://api.ipify.org?format=json", function(data) {
+    $.getJSON("https://api.specifix.dev/api/ip", function(data) {
         document.getElementById("ipText").innerText = ` ${data.ip}`;
     });
+
+    let hour = Number(new Date().toLocaleString('en-US', {hour12: false}).split(", ")[1].split(":")[0]);
+    if (hour < 18 && hour > 6) {
+        document.getElementById("goodday").innerText = `Have a good day!`;
+    } else {
+        document.getElementById("goodday").innerText = `Have a good night!`;
+    }
+
+    let sendButton = document.getElementById("sendButton")
+    let messagebox = document.getElementById("message")
+
+    sendButton.addEventListener("click", function() {
+        sendMessage(messagebox)
+    });
+    
     document.getElementById("prYear").innerText = `${(new Date()).getFullYear() - 2018} years`
     if (navigator.userAgent.match(/Android/i)
         || navigator.userAgent.match(/webOS/i)
@@ -17,3 +32,20 @@ $(document).ready(function() {
         document.getElementById("project-text-wrapper").style.width = "35%";
     }
 });
+
+
+function sendMessage(messagebox) {
+    var post = $.ajax({
+        type: "POST",
+        url: "https://api.specifix.dev/api/mailbox",
+        data: { content: messagebox.value },
+        dataType: "application/json"
+    })
+
+    post.done(async (data) => {
+        messagebox.placeholder = "Done!"
+        setTimeout(function() {
+            messagebox.placeholder = "Send me a message anonymously"
+        }, 1000);
+    })
+}
