6065f813a55b253bda732650446f9bd1eda014be
Specifix5 <danishkeenan23@gmail.com>
2024-05-22 11:20:55 +0700

    NEW WEB DESIGN

--- index.js
+++ index.js
@@ -1,53 +1,23 @@
-let sendMessageDEFAULT = `Send me a message anonymously`
+let sendMessageDEFAULT = `Message me anonymously!`
 
-$(document).ready(function() { 
-    document.getElementById("prYear").innerText = `${(new Date()).getFullYear() - 2018} years`
-    let sendButton = document.getElementById("sendButton")
-    let messagebox = document.getElementById("message")
+function debounce(method, delay) {
+    clearTimeout(method._tId);
+    method._tId= setTimeout(function(){
+        method();
+    }, delay);
+}
 
-    let hour = Number(new Date().toLocaleString('en-US', {hour12: false}).split(", ")[1].split(":")[0]);
-    if (navigator.userAgent.match(/Android/i)
-        || navigator.userAgent.match(/webOS/i)
-        || navigator.userAgent.match(/iPhone/i)
-        || navigator.userAgent.match(/iPad/i)
-        || navigator.userAgent.match(/iPod/i)
-        || navigator.userAgent.match(/BlackBerry/i)
-        || navigator.userAgent.match(/Windows Phone/i)) {
-            document.getElementById("avatar_img").style.marginLeft = "10px";
-            document.getElementById("project-text-wrapper").style.width = "100%";
-            sendMessageDEFAULT = `Send me a message`
-            messagebox.placeholder = sendMessageDEFAULT
-    } else {
-        document.getElementById("avatar_img").style.marginLeft = "75px";
-        document.getElementById("project-text-wrapper").style.width = "35%";
-    }
+function xMouseOver() {
+    var element = document.querySelector('.social-twitter > i')
+    element.classList.remove('fa-twitter')
+    element.classList.add('fa-x-twitter')
+}
 
-    if (hour < 18) {
-        if (hour > 5) {
-            document.getElementById("goodday").innerText = `have a good day!`;
-        } else {
-            document.getElementById("goodday").innerText = `have a good night!`;
-        }
-    } else {
-        document.getElementById("goodday").innerText = `have a good night!`;
-    }
-
-    sendButton.addEventListener("click", function() {
-        sendMessage(messagebox)
-    });
-
-    try {
-        $.getJSON("https://api.specifix.dev/api/ip", function(data) {
-            document.getElementById("ipText").innerText = ` ${data.ip}`;
-        });
-
-        $.getJSON("https://api.specifix.dev/api/subcount", function(data) {
-            document.getElementById("subcount").innerText = `${data.subCount}`;
-        })
-    } catch (err) {
-        console.log(err);
-    }
-});
+function xMouseOut() {
+    var element = document.querySelector('.social-twitter > i')
+    element.classList.remove('fa-x-twitter')
+    element.classList.add('fa-twitter')
+}
 
 function sendMessage(messagebox) {
     var post = $.ajax({
@@ -57,7 +27,7 @@ function sendMessage(messagebox) {
         contentType: "application/json",
         success: function(response) {
             messagebox.value = ""
-            messagebox.placeholder = "Done!"
+            messagebox.placeholder = "Sent!"
             setTimeout(function() {
                 messagebox.placeholder = sendMessageDEFAULT
             }, 1500)
@@ -77,3 +47,74 @@ function sendMessage(messagebox) {
         }
     })
 }
+
+$(document).ready(function() { 
+    document.getElementById("year").innerText = `${(new Date()).getFullYear() - 2018} years`
+
+    if (window.innerWidth > 860) {
+        header.style.backgroundImage = "linear-gradient(180deg, #0f141d, #0f141d00)"
+        header.style.boxShadow = "0px 0px"
+    }
+
+    let hour = Number(new Date().toLocaleString('en-US', {hour12: false}).split(", ")[1].split(":")[0]);
+
+    if (hour < 18) {
+        if (hour < 1) {
+            document.getElementById("goodday").innerText = `Evening`;
+        } else if(hour > 11) {
+            document.getElementById("goodday").innerText = `Afternoon`;
+        } else {
+            document.getElementById("goodday").innerText = `Morning`;
+        }
+    } else {
+        document.getElementById("goodday").innerText = `Evening`;
+    }
+
+    try {
+        $.getJSON("https://api.specifix.dev/api/ip", function(data) {
+            document.getElementById("ip").textContent = ` ${data.ip}`;
+        });
+
+        $.getJSON("https://api.specifix.dev/api/subcount", function(data) {
+            document.getElementById("ytcount").innerText = `${data.subCount}`;
+        })
+    } catch (err) {
+        console.log(err);
+    }
+
+    let sendButton = document.getElementById("sendmessage")
+    let messagebox = document.getElementById("messagebox")
+
+    sendButton.addEventListener("click", function() {
+        sendMessage(messagebox)
+    });
+    
+    var element = document.getElementById("mainpagecontainer")
+    element.addEventListener('scroll', event => {
+        debounce(() => {
+            var header = document.getElementById("header")
+            var logotext = document.getElementById("logotext");
+            var l_iptext = document.getElementById("logoiptext");
+            if (window.innerWidth > 900) {
+                var scroll = element.scrollTop/(window.innerHeight)//element.scrollTop/(element.scrollHeight - window.innerHeight)
+                if (scroll > 0.85) { // SHOW
+                    header.style.backgroundColor = "#151d29"
+                    header.style.boxShadow = "3px 3px 3px #0c0f14b2"
+                    logotext.classList.remove("centerlogotext")
+                    l_iptext.classList.remove("centerlogotextremove")
+                } else {
+                    header.style.backgroundColor = "#151d2900"
+                    header.style.boxShadow = "0px 0px"
+                    logotext.classList.add("centerlogotext")
+                    l_iptext.classList.add("centerlogotextremove")
+                }
+            } else { // SHOW
+                header.style.backgroundColor = "#151d29"
+                header.style.boxShadow = "3px 3px 3px #0c0f14b2"
+                logotext.classList.remove("centerlogotext")
+                l_iptext.classList.remove("centerlogotextremove")
+            } 
+        }, 100)
+    });
+});
+
