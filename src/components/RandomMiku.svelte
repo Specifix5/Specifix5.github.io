ba2be3c9c6019f0b0ef27fb989ff396a4137e09e
Specifix <62927726+Specifix5@users.noreply.github.com>
2025-01-21 08:05:16 +0800

    Update RandomMiku.svelte

--- src/components/RandomMiku.svelte
+++ src/components/RandomMiku.svelte
@@ -5,12 +5,18 @@
     `<img draggable="false" src="/images/miku_plush.png" alt="m" class="emoji">`,
     `<img draggable="false" src="/images/miku_comfy.png" alt="m" class="emoji">`,
     `<img draggable="false" src="/images/miku_smirk.png" alt="m" class="emoji">`
-  ]
-  let html_string = "";
-  for (let i = 0; i < 4; i++) {
-    let miku = mikus[Math.floor(Math.random() * mikus.length)];
+  ];
 
-    html_string += miku;
+  // Reactive statement to regenerate `html_string`
+  $: html_string = generateRandomMikus();
+
+  function generateRandomMikus() {
+    let html = "";
+    for (let i = 0; i < 4; i++) {
+      let miku = mikus[Math.floor(Math.random() * mikus.length)];
+      html += miku;
+    }
+    return html;
   }
 </script>
 
