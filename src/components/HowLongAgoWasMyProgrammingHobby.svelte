239807768680ab2892d03af5343d8a880bd6f818
Specifix5 <62927726+Specifix5@users.noreply.github.com>
2025-04-07 02:02:52 +0800

    refactor some stuff

--- src/components/HowLongAgoWasMyProgrammingHobby.svelte
+++ src/components/HowLongAgoWasMyProgrammingHobby.svelte
@@ -5,7 +5,7 @@
     setInterval(() => {
     let relativeTime = Date.now() - howLongAgoWasIt;
     document.getElementById("long-ago")!.innerText = `${relativeTime / 1_000 / 60 / 60 / 24 / 30 / 12} years`; // totally a random day in march
-  }, 10_000)
+  }, 1_000)
   })
 </script>
 
